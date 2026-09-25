2f24039967bfde339991d7309f8a99f6d29f8b70
L0rdDioBrando <kazukiobsidian@gmail.com>
2026-09-25 18:46:42 +0500

    Полностью переписан

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     uv2nix.url = "github:pyproject-nix/uv2nix";
     pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
     pyproject-build-systems.url = "github:pyproject-nix/build-system-pkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
@@ -15,91 +16,42 @@
       uv2nix,
       pyproject-nix,
       pyproject-build-systems,
+      flake-utils,
       ...
     }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-      ];
-      forEachSupportedSystem =
-        f:
-        nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          let
-            pkgs = import nixpkgs { inherit system; };
-            tclLib = "${pkgs.tcl}/lib/tcl${pkgs.lib.versions.majorMinor pkgs.tcl.version}";
-            tkLib = "${pkgs.tk}/lib/tk${pkgs.lib.versions.majorMinor pkgs.tk.version}";
-            tkinterPath = "${pkgs.python3Packages.tkinter}/${pkgs.python3.sitePackages}";
-          in
-          f {
-            inherit
-              pkgs
-              tclLib
-              tkLib
-              tkinterPath
-              ;
-          }
-        );
-    in
-    {
-      devShells = forEachSupportedSystem (
-        {
-          pkgs,
-          tclLib,
-          tkLib,
-          tkinterPath,
-        }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              tk
-              tcl
-              stdenv.cc.cc.lib
-              zlib
-              gnumake
-              uv
-            ];
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
 
-            shellHook = ''
-              python3 -m venv .venv
-              unset PYTHONPATH
-              export TCL_LIBRARY="${tclLib}"
-              export TK_LIBRARY="${tkLib}"
-              export PYTHONPATH="${tkinterPath}"
-            '';
-          };
-        }
-      );
+        uvOverlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "wheel";
+        };
 
-      packages = forEachSupportedSystem (
-        {
-          pkgs,
-          tclLib,
-          tkLib,
-          tkinterPath,
-        }:
-        {
-          default = pkgs.stdenv.mkDerivation {
-            pname = "skin-gacha";
-            version = "0.5.0";
-            src = ./.;
+        pythonSet =
+          (pkgs.callPackage pyproject-nix.build.packages {
+            python = pkgs.python3;
+          }).overrideScope
+            (
+              pkgs.lib.composeManyExtensions [
+                pyproject-build-systems.overlays.default
+                uvOverlay
+              ]
+            );
 
-            nativeBuildInputs = with pkgs; [
-              makeWrapper
-              python3
-            ];
-
-            installPhase = ''
-              python3 -m venv .venv
-              mkdir -p $out/bin
-              makeWrapper $out/bin/python $out/bin/skin-gacha \
-                --add-flags "$src/main.py" \
-                --prefix PYTHONPATH : "${tkinterPath}:$src" \
-                --set TCL_LIBRARY "${tclLib}" \
-                --set TK_LIBRARY "${tkLib}"
-            '';
-          };
-        }
-      );
-    };
+        virtualenv = pythonSet.mkVirtualEnv "dev-env" workspace.deps.all;
+      in
+      {
+        packages.default = virtualenv;
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.gnumake
+            pkgs.ninja
+            virtualenv
+            pkgs.uv
+          ];
+        };
+      }
+    );
 }
